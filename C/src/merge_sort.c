e4f89fbbed53bb251f48dd8cf837c6879b447e3d
Z1nnlos <noel.sabia@gmx.net>
2024-03-28 22:20:00 +0100

    did some testing

--- C/src/merge_sort.c
+++ C/src/merge_sort.c
@@ -1,12 +1,25 @@
 #include "../includes/C-Algo.h"
 
+long long int	*split_arr(long long int *dest, long long int *src, int start, int end)
+{
+	while (start <= end)
+	{
+		dest[start] = src[start];
+		start++;
+	}
+	return (dest);
+}
+
 void	*merge_sort(void *array)
 {
 	s_threadinfo *threadinfo;
 	float	middle;
+	long long int *temp_arr1;
+	long long int *temp_arr2;
+	int		*solved_arr;
 
 	if (threadinfo->len == 1)
-		return (threadinfo->arr);
+		return ((void *)array);
 	else
 	{
 		middle = (float)threadinfo->len / 2;
@@ -14,14 +27,16 @@ void	*merge_sort(void *array)
 		if (remainder == 0.5)
 		{
 			middle += 0.5;
-			char *temp_arr1 = memcpy(temp_arr1, threadinfo->arr, middle);
-			char *temp_arr2 = memcpy(temp_arr2, threadinfo->arr, middle - 1); //TODO: not working like that would copy both the same part of arr
+			temp_arr1 = split_arr(temp_arr1, array, 0, middle);
+			temp_arr2 = split_arr(temp_arr2, array, middle + 1, threadinfo->len);
 		}
 		else
 		{
-			char *temp_arr1 = memcpy(temp_arr1, threadinfo->arr, middle);
-			char *temp_arr2 = memcpy(temp_arr2, threadinfo->arr, middle - 1);	
+			temp_arr1 = split_arr(temp_arr1, array, 0, middle);
+			temp_arr2 = split_arr(temp_arr2, array, middle + 1, threadinfo->len);
 		}
+		long long int *left_arr = merge_sort((void *)temp_arr1);
+		long long int *right_arr = merge_sort((void *)temp_arr2);
 	}
-	return NULL;
+	return ((void *)solved_arr);
 }
