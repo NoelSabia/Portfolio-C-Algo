2cefb42769b72de5ed76ff797c10d87f35f858eb
Z1nnlos <Noel.Sabia@gmx.net>
2024-04-03 23:38:16 +0200

    the arrays are sorted and back in a 2D array home_arr now only mergen them and in one file

--- C/src/merge_sort.c
+++ C/src/merge_sort.c
@@ -3,44 +3,44 @@
 long long int *merge(long long int *arr, size_t left, size_t middle, size_t right)
 {
 	int i, j, k;
-    int n1 = middle - left + 1;
-    int n2 = right - middle;
-    int L[n1], R[n2];
+	int n1 = middle - left + 1;
+	int n2 = right - middle;
+	int L[n1], R[n2];
 
-    for (i = 0; i < n1; i++)
-        L[i] = arr[left + i];
-    for (j = 0; j < n2; j++)
-        R[j] = arr[middle + 1 + j];
+	for (i = 0; i < n1; i++)
+		L[i] = arr[left + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[middle + 1 + j];
 
-    i = 0;
-    j = 0;
-    k = left;
-    while (i < n1 && j < n2)
+	i = 0;
+	j = 0;
+	k = left;
+	while (i < n1 && j < n2)
 	{
-        if (L[i] <= R[j])
+		if (L[i] <= R[j])
 		{
-            arr[k] = L[i];
-            i++;
-        }
-        else
+			arr[k] = L[i];
+			i++;
+		}
+		else
 		{
-            arr[k] = R[j];
-            j++;
-        }
-        k++;
-    }
-    while (i < n1)
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+	while (i < n1)
 	{
-        arr[k] = L[i];
-        i++;
-        k++;
-    }
-    while (j < n2)
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+	while (j < n2)
 	{
-        arr[k] = R[j];
-        j++;
-        k++;
-    }
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
 	return (arr);
 }
 
@@ -68,14 +68,6 @@ void	*merge_sort_main(void *threadinfo)
 	long long int	*arr = thread->arr;
 	long long int	*solved;
 
-	// printf("\n");
-	// for (int j = 0; j < thread->len; j++)
-	// 	printf("before[%d]: %lld\n", j, thread->arr[j]);
-	// printf("\n");
 	solved = mergeSort(arr, 0, thread->len, thread->len);
-	// printf("\n");
-	// for (int k = 0; k < thread->len; k++)
-	// 	printf("solved[%d]: %lld\n", k, solved[k]);
-	// printf("\n");
 	return ((void *)solved);
 }
--- C/src/threads.c
+++ C/src/threads.c
@@ -2,17 +2,15 @@
 
 static int temp_arr_len;
 
-long long int *arr_for_threads(s_algo *algo)
+long long int *arr_for_threads(s_algo *algo, int *start, int *end)
 {
-	static int start = 0;
-	static int end = 0;
 	static int	repetition = 1;
 	int division = algo->home_arr_len / algo->threads;
 	long long int *temp_arr;
 	int i;
 
-	if (start == 0)
-		end = division;
+	if (*start == 0)
+		*end = division;
 	algo->division = division;
 	temp_arr = malloc(sizeof(long long int) * (division + 10));
 	i = 0;
@@ -24,26 +22,36 @@ long long int *arr_for_threads(s_algo *algo)
 	}
 	if (repetition < algo->threads)
 	{
-		while(start <= end)
+		while(*start < *end)
 		{
-			temp_arr[i] = *algo->home_arr[start];
+			temp_arr[i] = *(algo->home_arr[*start]);
 			i++;
-			start++;
+			(*start)++;
 		}
-		end += division;
+		*end += division;
+		temp_arr_len = division;
 	}
 	else
 	{
-		while(start < algo->home_arr_len)
+		while(*start < algo->home_arr_len)
 		{
-			temp_arr[i] = *algo->home_arr[start];
+			temp_arr[i] = *(algo->home_arr[*start]);
 			i++;
-			start++;
+			(*start)++;
 		}
+		temp_arr_len = algo->home_arr_len - (*start - i);
 	}
 	repetition++;
-	temp_arr_len = start;
 	return (temp_arr);
+}
+
+void put_into_2d_array(s_algo *algo, s_threadinfo *threadinfo, long long int *sorted_arr, int index)
+{
+	int len = threadinfo->len;
+	for (int i = 0; i <= len; i++)
+	{
+		algo->home_arr[index][i] = sorted_arr[i];
+	}
 }
 
 void	threads(s_algo *algo, int threads)
@@ -53,18 +61,16 @@ void	threads(s_algo *algo, int threads)
 	int				i;
 	int				result;
 	long long int	*temp_arr;
+	int				start = 0;
+	int				end = 0;
 
 	for (i = 0; i < threads; i++)
 	{
-		temp_arr = arr_for_threads(algo);
+		temp_arr = arr_for_threads(algo, &start, &end);
 		threadinfo = malloc(sizeof(s_threadinfo));
 		threadinfo->arr = malloc(sizeof(long long int) * temp_arr_len);
 		memcpy(threadinfo->arr, temp_arr, sizeof(long long int) * temp_arr_len);
 		threadinfo->len = temp_arr_len;
-		printf("\n");
-		for (int k = 0; k < threadinfo->len; k++)
-			printf("arr[%d]: %lld\n", k, threadinfo->arr[k]);
-		printf("\n");
 		result = pthread_create(&thread_id[i], NULL, merge_sort_main, (void *)threadinfo);
 		if (result != 0)
 		{
@@ -73,17 +79,18 @@ void	threads(s_algo *algo, int threads)
 			return ;
 		}
 		free(temp_arr);
-		usleep(10);//TODO: Dont forget to take that away
 	}
 	for (i = 0; i < threads; i++)
 	{
-		result = pthread_join(thread_id[i], NULL);
-		//TODO: Dont forget to free threadinfo
-		printf("joined\n");
+		void *thread_result;
+		result = pthread_join(thread_id[i], &thread_result); //TODO: Dont forget to free threadinfo
 		if (result != 0)
 		{
 			err_message("Thread join failed!");
 			return ;
 		}
+		long long int *sorted_arr = (long long int *)thread_result;
+		put_into_2d_array(algo, threadinfo, sorted_arr, i);
 	}
+	//exit(0); TODO: only the exit() stops you from having this error: free(): invalid pointer Aborted
 }
